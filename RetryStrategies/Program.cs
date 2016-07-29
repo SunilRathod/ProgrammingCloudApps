10d9c415f1407f15602a18c4376afdb881cfb93b
Sunil Rathod <sunil.rathod@outlook.com>
2016-07-29 13:53:15 +0000

    Add files via upload

---
+++ RetryStrategies/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetryStrategies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the url of a site.");
+            var url = Console.ReadLine();
+            PrintSiteHtml(url);
+            Console.WriteLine("Press any key to exit.");
+            Console.Read();
+        }
+
+        public static void PrintSiteHtml(string url)
+        {
+            HttpClient httpClient = new HttpClient();
+            string html = string.Empty;
+
+            RetryStrategy.TightLoop(5, () =>
+            {
+                HttpResponseMessage response = httpClient.GetAsync(url).Result;
+                if (!response.IsSuccessStatusCode) throw new ApplicationException("Could not connect");
+                html = response.Content.ReadAsStringAsync().Result;
+            });
+
+            RetryStrategy.ConstantTimeInterval(10, 2, () =>
+            {
+                HttpResponseMessage response = httpClient.GetAsync(url).Result;
+                if (!response.IsSuccessStatusCode) throw new ApplicationException("Could not connect");
+                html = response.Content.ReadAsStringAsync().Result;
+            });
+
+            RetryStrategy.RandomInterval(10, 1, 5, () =>
+            {
+                HttpResponseMessage response = httpClient.GetAsync(url).Result;
+                if (!response.IsSuccessStatusCode) throw new ApplicationException("Could not connect");
+                html = response.Content.ReadAsStringAsync().Result;
+            });
+
+            RetryStrategy.ExponentialBackOff(10, 1, 10, () =>
+            {
+                HttpResponseMessage response = httpClient.GetAsync(url).Result;
+                if (!response.IsSuccessStatusCode) throw new ApplicationException("Could not connect");
+                html = response.Content.ReadAsStringAsync().Result;
+            });
+
+            Console.WriteLine(html);
+        }
+    }
+}
---
+++ RetryStrategies/RetryStrategy.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Threading;
+
+namespace RetryStrategies
+{
+    public static class RetryStrategy
+    {
+        private const int Second = 1000;
+
+        public static void TightLoop(int retryAttempts, Action executeLogic)
+        {           
+            int currentAttemptNumber = 0;
+            do
+            {
+                try
+                {
+                    executeLogic();
+                    break;
+                }
+                catch (Exception ex)
+                {                    
+                    if (++currentAttemptNumber >= retryAttempts) break;
+                    Console.WriteLine("Attempt failed.");                   
+                    Console.WriteLine(string.Format("Tight Loop: Retrying attempt: {0}/{1} at time: {2} seconds.", currentAttemptNumber, retryAttempts, DateTime.Now));
+                }
+            } while (true);
+        }
+
+        public static void ConstantTimeInterval(int retryAttempts, int interval, Action executeLogic)
+        {                       
+            int currentAttemptNumber = 0;
+            do
+            {
+                try
+                {
+                    executeLogic();
+                    break;
+                }
+                catch (Exception ex)
+                {                    
+                    if (++currentAttemptNumber >= retryAttempts) break;
+                    Console.WriteLine("Attempt failed.");
+                    Console.WriteLine(string.Format("Constant Loop: Retrying attempt {0}/{1} at time {2} with an interval of {3} seconds.", currentAttemptNumber, retryAttempts, DateTime.Now, interval));
+                    Thread.Sleep(interval * Second);
+                }
+            } while (true);
+        }
+
+        public static void RandomInterval(int retryAttempts, int intervalMin, int intervalMax, Action executeLogic)
+        {
+            Random randomValueGenerator = new Random();           
+            int minInterval = intervalMin;
+            int maxInterval = intervalMax;
+            int interval = 0;
+            int currentAttemptNumber = 0;
+
+            do
+            {
+                try
+                {
+                    executeLogic();
+                    break;
+                }
+                catch (Exception ex)
+                {                    
+                    if (++currentAttemptNumber >= retryAttempts) break;
+                    Console.WriteLine("Attempt failed.");
+                    Console.WriteLine(string.Format("Random interval Loop: Retrying attempt {0}/{1} at time {2} with an interval of {3} seconds.", currentAttemptNumber, retryAttempts, DateTime.Now, interval));
+                    interval = randomValueGenerator.Next(minInterval, maxInterval);
+                    Thread.Sleep(interval * Second);
+                }
+            } while (true);
+        }
+
+        public static void ExponentialBackOff(int retryAttempts, int minInterval, int maxInterval, Action executeLogic)
+        {
+                    
+            int interval = minInterval;
+            int exponent = 2;
+            int currentAttemptNumber = 0;
+            do
+            {
+                try
+                {
+                    executeLogic();
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    if (++currentAttemptNumber >= retryAttempts) break;
+                    Console.WriteLine("Attempt failed.");
+                    Console.WriteLine(string.Format("Exponential Backoff Loop: Retrying attempt {0}/{1} at time {2} with an interval of {3} seconds.", currentAttemptNumber, retryAttempts, DateTime.Now, interval));
+                    Thread.Sleep(interval * Second);
+                    interval = Math.Min(maxInterval, interval * exponent);
+                }
+            } while (true);
+
+        }
+    }
+}
